3fe38ff3f446fcfc6a4a91cf5949703b585ee130
dylang4000 <davis948@student.pcsstn.net>
2025-03-26 09:40:39 -0500

    moves some files

--- scripting/downloadsScript.py
+++ scripting/downloadsScript.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import operator
+import shutil
 
 from getpass import getuser
 
@@ -9,6 +11,15 @@ DownloadsDirectories=[]
 
 os.chdir(userpath)
 
+for i in os.listdir():
+    if "." in i:
+        DownloadsFiles.append(i)
+    elif ("." in i)==False:
+        DownloadsDirectories.append(i)
 
-        
 
+for i in DownloadsFiles:
+    if operator.contains(i,".png") or operator.contains(i,".jpg") or operator.contains(i,".ico") or operator.contains(i,".webp") :
+            shutil.move(f"{userpath}\{i}",r"C:\Users\Dylan Davis\Pictures\downloaded images")
+    elif operator.contains(i,".zip") or operator.contains(i,".7z"):
+         shutil.move(f"{userpath}\{i}",r"C:\Users\Dylan Davis\Documents\Compressed files")
