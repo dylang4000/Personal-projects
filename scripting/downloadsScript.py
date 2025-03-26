1b6650c33695c0a29417f4af352dc207416258c4
dylang4000 <davis948@student.pcsstn.net>
2025-03-26 03:58:33 -0700

    asd

--- scripting/downloadsScript.py
+++ scripting/downloadsScript.py
@@ -1,6 +1,14 @@
 import sys
+import os
 
 from getpass import getuser
 
-userpath = fr"C:\Users\{getuser()}"
+userpath = fr"C:\Users\{getuser()}\Downloads"
+DownloadsFiles=[]
+DownloadsDirectories=[]
+
+os.chdir(userpath)
+
+
+        
 
